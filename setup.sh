14dde7c089137ae42dcde0f69e10a2ab02d328a2
BPDanek <bdanek@asu.edu>
2020-11-11 16:42:59 -0700

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,14 @@
 # check parameter count 
-if [ "$#" -ne 1 ]; then
-    echo "Error: Not enough parameters"
-    echo "Usage: ./setup /absolute/path/to/AFL/"
+if [ "$#" -ne 2 ]; then
+    echo "Error: Not enough parameters, got $1 and $2"
+    echo "Usage: ./setup AFL_LOCATION binary"
     exit 1
 fi
 
+echo "\nWEVE MADE IT THIS FAR\n"
+
 # make build loc
-mkdir build
+ mkdir build
 
 # download requirements
 apt install cmake -y
@@ -22,4 +24,4 @@ sleep 2
 (cd mavlink/pymavlink/ && tools/mavgen.py --lang=C --wire-protocol=2.0 --output=../../generated/include/mavlink/v2.0 ../message_definitions/v1.0/common.xml)
 
 # compile the harness
-$AFL_DIR/afl-g++ -I generated/include/mavlink/v2.0 -static src/single_byte_harness.cpp -o build/single_byte_harness 
+$AFL_DIR/afl-g++ -I generated/include/mavlink/v2.0 -static src/$2 -o /phuzzui/build/$2
